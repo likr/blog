725ac9544da3f31975284361f6cd0073e6e64f21
Yosuke ONOUE <onoue@likr-lab.com>
2016-07-05 09:02:22 +0900

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     filename: './assets/js/bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   devtool: '#inline-source-map'
 }
